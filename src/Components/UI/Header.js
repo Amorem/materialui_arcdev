f39724303a83b7c92ce1854edf171f4c5af170c7
Amorem <dev@amorem.com>
2020-04-13 17:21:21 +0200

    Header finished

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -24,7 +24,7 @@ function ElevationScroll({ children }) {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em",
+    marginBottom: "2em",
   },
   logo: {
     height: "7em",
@@ -44,17 +44,42 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "50px",
     height: "45px",
   },
+  logoContainer: {
+    padding: 0,
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
   return (
     <>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img className={classes.logo} src={logo} alt="company logo" />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img className={classes.logo} src={logo} alt="company logo" />
+            </Button>
             <Tabs
               className={classes.tabContainer}
               value={value}
