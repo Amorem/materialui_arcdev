4f1355b1c373d58db021cbc903027668d80f663e
Amorem <dev@amorem.com>
2020-04-22 11:54:32 +0200

    Information block

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -14,6 +14,7 @@ import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
 import revolutionBackground from "../assets/repeatingBackground.svg";
+import infoBackground from "../assets/infoBackground.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -111,6 +112,14 @@ const useStyles = makeStyles((theme) => ({
       width: "100%",
     },
   },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
 }));
 
 export default function LandingPage() {
@@ -166,7 +175,7 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
-        {/*----- Software Block -----*/}
+        {/*----- Custom Software Block -----*/}
         <Grid
           container
           direction="row"
@@ -287,6 +296,7 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
+        {/*----- Revolution Block -----*/}
         <Grid
           container
           alignItems="center"
@@ -326,6 +336,35 @@ export default function LandingPage() {
             </CardContent>
           </Card>
           <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*----- Information Block -----*/}
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          style={{ height: "80em" }}
+        >
+          <Grid item style={{ position: "absolute", marginLeft: "5em" }}>
+            <Grid container direction="column">
+              <Typography variant="h2" style={{ color: "white" }}>
+                About Us
+              </Typography>
+              <Typography variant="subtitle2">Let's get personal.</Typography>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  className={classes.learnButton}
+                  style={{ color: "white", borderColor: "white" }}
+                >
+                  <span style={{ marginRight: 10 }}> Learn more</span>
+                  <ButtonArrow width="10" height="10" fill="white" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.infoBackground}></div>
         </Grid>
       </Grid>
     </Grid>
