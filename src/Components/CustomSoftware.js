6eb84b10e87aaff85be884d84f60c0b22541fd96
Amorem <dev@amorem.com>
2020-04-24 17:36:20 +0200

    Page with animations

--- src/Components/CustomSoftware.js
+++ src/Components/CustomSoftware.js
@@ -12,6 +12,12 @@ import forwardArrow from "../assets/forwardArrow.svg";
 import lightbulb from "../assets/bulb.svg";
 import cash from "../assets/cash.svg";
 import stopwatch from "../assets/stopwatch.svg";
+import roots from "../assets/root.svg";
+
+import documentsAnimation from "../animations/documentsAnimation/data";
+import scaleAnimation from "../animations/scaleAnimation/data";
+import automationAnimation from "../animations/automationAnimation/data";
+import uxAnimation from "../animations/uxAnimation/data";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -26,10 +32,49 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: "2em",
     paddingBottom: "10em",
   },
+  itemContainer: {
+    maxWidth: "40em",
+  },
 }));
 
 export default function CustomSoftware(props) {
   const classes = useStyles();
+  const documentsOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: documentsAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const scaleOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const automationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: automationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const uxOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: uxAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <Grid container direction="column" className={classes.mainContainer}>
       <Grid item container direction="row">
@@ -144,6 +189,159 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item>
             <img src={cash} alt="cash" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction="row" justify="space-between">
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                The decade that brought us Star Trek and Doctor Who also
+                resurrected Cicero—or at least what used to be Cicero—in an
+                attempt to make the days before computerized design a little
+                less painstaking.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                The French lettering company Letraset manufactured a set of
+                dry-transfer sheets which included the lorem ipsum filler text
+                in a variety of fonts, sizes, and layouts.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Aldus Corporation, which later merged with Adobe Systems,
+                ushered lorem ipsum into the information age with its desktop
+                publishing software Aldus PageMaker.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              style={{ minHeight: 250, maxHeight: 275, maxWidth: 275 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item md>
+              <Lottie
+                options={scaleOptions}
+                style={{ maxHeight: 260, maxWidth: 280 }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                Scale
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                And that’s why a 15th century typesetter might have scrambled a
+                passage of Cicero; he wanted people to focus on his fonts, to
+                imagine their own content on the pages. He wanted people to see,
+                and to get them to see he had to keep them from reading.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="row"
+        style={{ marginTop: "20em", marginBottom: "20em" }}
+      >
+        <Grid item container direction="column" alignItems="center">
+          <Grid item>
+            <img
+              src={roots}
+              alt="tree with roots extending out"
+              height="450em"
+              width="450"
+            />
+          </Grid>
+          <Grid item className={classes.itemContainer}>
+            <Typography variant="h4" gutterBottom align="center">
+              Root-Cause Analysis
+            </Typography>
+            <Typography variant="body1" paragraph align="center">
+              Coming full circle, the internet's remixing of the now infamous
+              lorem ipsum passage has officially elevated it to pop culture
+              status.
+            </Typography>
+            <Typography variant="body1" paragraph align="center">
+              One word of caution: make sure your client knows that lorem ipsum
+              is filler text. You don't want them wondering why you filled their
+              website with a foreign language, and you certainly don't want
+              anyone prematurely publishing it.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction="row" justify="space-between">
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                If you haven't seen Game of Thrones, go watch it right now. If
+                you have then you'll totally get why this Hodor themed lorem
+                ipsum generator is just brilliant.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                In case you don't read Twitter, the news, or just can't get
+                enough of The Apprentice host's legendary oration, try this
+                Trump lorem ipsum generator on for size.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                A handcrafted, small-batch, artisinal pour-over version of the
+                classic lorem ipsum generator, Hipster Ipsum will give your
+                mocks that blue collar touch.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={automationOptions}
+              style={{ maxHeight: 290, maxWidth: 250 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                User Experience Design
+              </Typography>
+            </Grid>
+            <Grid item md>
+              <Lottie
+                options={uxOptions}
+                style={{ maxHeight: 310, maxWidth: 155 }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                Raise your design from the dead with an army of Zombie Ipsum,
+                frightful filler text that just won't die. Try the lorem ipsum
+                of the undead if you dare...
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                Like your lorem ipsum extra crispy? Then Bacon Ipsum is the
+                placeholder text generator for you. Side of eggs and hashbrowns
+                is optional, but recommended.
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                Sugary sweet lorem ipsum? You got it with Cupcake Ipsum, the
+                only text generator that includes marshmallows, carrot cake, and
+                perhaps even a cherry on top.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
