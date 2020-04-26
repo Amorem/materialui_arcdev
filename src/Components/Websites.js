568e9dd0b43ebc16046112024732c68cdecf76f3
Amorem <dev@amorem.com>
2020-04-26 10:13:24 +0200

    Websites page finished

--- src/Components/Websites.js
+++ src/Components/Websites.js
@@ -14,6 +14,8 @@ import seo from "../assets/seo.svg";
 import outreach from "../assets/outreach.svg";
 import ecommerce from "../assets/ecommerce.svg";
 
+import CallToAction from "./UI/CallToAction";
+
 const useStyles = makeStyles((theme) => ({
   heading: {
     maxWidth: "40em",
@@ -39,6 +41,7 @@ export default function Websites(props) {
   const theme = useTheme();
 
   const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
   const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
@@ -108,9 +111,10 @@ export default function Websites(props) {
       <Grid
         item
         container
-        direction="row"
+        direction={matchesSM ? "column" : "row"}
         className={classes.rowContainer}
         alignItems="center"
+        style={{ marginTop: "15em" }}
       >
         <Grid item>
           <Grid container direction="column">
@@ -128,13 +132,152 @@ export default function Websites(props) {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="body1" className={classes.paragraphContainer}>
+        <Grid item className={classes.paragraphContainer}>
+          <Typography variant="body1" align={matchesSM ? "center" : undefined}>
             Shred your next project's filler text by dropping in some skater
             lorem ipsum, letting it read like a scene straight out of the old
             Tony Hawk video games.
           </Typography>
         </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+        justify="flex-end"
+        style={{ marginBottom: "15em", marginTop: "15em" }}
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h4" gutterBottom align="center">
+                Ecommerce
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={ecommerce} alt="world outline made of dollar signs" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography
+            variant="body1"
+            paragraph
+            align={matchesSM ? "center" : undefined}
+          >
+            If you're an Arrested Development fan than this lorem ipsum
+            generator is for you. Make your project stand out with ramblings big
+            and small from Michael Bluth.
+          </Typography>
+          <Typography
+            variant="body1"
+            paragraph
+            align={matchesSM ? "center" : undefined}
+          >
+            Favor the more eloquent presidential days of yesteryear? Give Obama
+            Ipsum a whirl and get some engaging lorem ipsum text that wasn't
+            first seen on Twitter.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography
+                variant="h4"
+                gutterBottom
+                align={matchesSM ? "center" : undefined}
+              >
+                Outreach
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={outreach} alt="megaphone" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography variant="body1" align={matchesSM ? "center" : undefined}>
+            Want to generate lorem ipsum from nonsensical Web 2.0 startup names?
+            Relive the glory days where everything was rounded and covered in a
+            bright, gradated sheen.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+        justify="flex-end"
+        style={{ marginTop: "15em", marginBottom: "15em" }}
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h4" gutterBottom align="center">
+                Search Engine Optimization
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={seo} alt="website standing on winner's podium" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: matchesSM ? 0 : "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography
+            variant="body1"
+            paragraph
+            align={matchesSM ? "center" : undefined}
+          >
+            Ahoy matey, yar lorem ipsum text is awash with brigands, bilge and
+            more. Get swashbuckling with this trendy looking pirate placeholder
+            text generator.
+          </Typography>
+          <Typography
+            variant="body1"
+            paragraph
+            align={matchesSM ? "center" : undefined}
+          >
+            Channel your inner astronomer with this Carl Sagan inspired lorem
+            ipsum text generator. Discover the cosmos one sentence of paragraph
+            at a time.
+          </Typography>
+          <Typography
+            variant="body1"
+            paragraph
+            align={matchesSM ? "center" : undefined}
+          >
+            Based on the hit TV show Breaking Bad, Heisenberg Ipsum lets you
+            choose your favorite character and generates lorem ipsum from their
+            movie quotes.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue} />
       </Grid>
     </Grid>
   );
